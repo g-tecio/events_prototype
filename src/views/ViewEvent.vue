40f5d386ba4798628af5c4fceec39499afd1b0b9
sirzs <Diego_Zoso@hotmail.com>
2019-01-04 16:10:57 -0600

    View Event Fixed

--- src/views/ViewEvent.vue
+++ src/views/ViewEvent.vue
@@ -1,68 +1,100 @@
 <template>
   <v-container fill-height fluid grid-list-xl>
-    
     <v-layout justify-center wrap>
       <v-flex xs12 md12>
-          
-        <material-card color="blue" title="New Event" text="Create a new event">
+        <material-card color="blue" title="Event Details" text="About the event">
            <v-img :src="event.event_image" aspect-ratio="2"></v-img>
           <v-form>
-            <v-container py-0>
-              <v-layout wrap>
-                <v-flex xs12 md4>
+            <v-container grid-list-xl text-xs-center>
+              <v-layout row wrap>
+                <v-flex xs12 md6>
                   <v-text-field disabled label="Event Name" v-model="event.event_name"/>
                 </v-flex>
+                <v-flex xs12 md6>
+                  <v-text-field disabled label="Venue Name" v-model="event.venue_name"/>
+                </v-flex>
                 <!-- Addresss -->
-                <v-flex xs12 md8>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.address.line_address1" label="Line address 1"/>
                 </v-flex>
                 <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.address.line_address2" label="Line address 2"/>
                 </v-flex>
-                <v-flex xs12 md3>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.address.country" label="Country"/>
                 </v-flex>
-                <v-flex xs12 md3>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.address.state" label="State"/>
                 </v-flex>
-                <v-flex xs12 md4>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.address.city" label="City"/>
                 </v-flex>
-                <v-flex xs12 md2>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.address.zip_code" label="Zip code"/>
                 </v-flex>
+                 <v-flex xs12>
+                  <v-img disabled :aspect-ratio="16/9" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+                    <v-layout pa-2 column fill-height class="lightbox white--text">
+                      <v-spacer></v-spacer>
+                      <v-flex shrink>
+                        <div class="subheading">Add Location</div>
+                      </v-flex>
+                    </v-layout>
+                  </v-img>
+                </v-flex>
+              </v-layout>
+
+              <v-layout row wrap>
+                <!-- Event generalities -->
                 <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.address.description" label="Address Description"/>
                 </v-flex>
-
-                <!-- Event generalities -->
-                <v-flex xs12 md3>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.event_type" label="Event Type"/>
                 </v-flex>
-                <v-flex xs12 md3>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.event_attire" label="Attire"/>
                 </v-flex>
-                <v-flex xs12 md4>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.revenue_generation" label="Revenue generation"/>
                 </v-flex>
-                <v-flex xs12 md2>
+                <v-flex xs12 md6>
                   <v-text-field disabled v-model="event.capacity" label="Capacity"/>
                 </v-flex>
                 
                 <v-flex xs12>
                   <v-textarea disabled v-model="event.description" label="Description"/>
                 </v-flex>
-                <v-flex xs12 md9>
-                  <v-date-picker
-                    class="dark-text"
-                    color="blue"
-                    v-model="event.date.formated_date"
-                    :landscape="landscape"
-                    :reactive="reactive"
-                    :disabled="true"
-                  ></v-date-picker>
+                <v-flex xs12 md6>
+                  <v-menu
+                    ref="menu"
+                    :close-on-content-click="false"
+                    v-model="menu"
+                    :nudge-right="40"
+                    :return-value.sync="date"
+                    lazy
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    min-width="290px"
+                    disabled
+                  >
+                    <v-text-field
+                      slot="activator"
+                      v-model="event.date.formated_date"
+                      label="Start Date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      disabled
+                    ></v-text-field>
+                    <v-date-picker v-model="event.date.formated_date" no-title scrollable>
+                      <v-spacer></v-spacer>
+                      <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+                      <v-btn flat color="primary" @click="$refs.menu.save(event.date.formated_date)">OK</v-btn>
+                    </v-date-picker>
+                  </v-menu>
                 </v-flex>
-                <v-flex xs12 md3>
+                <v-flex xs12 md6>
                   <v-select disabled v-model="event.status" :items="status" item-text="event.status" ></v-select>
                 </v-flex>
 
@@ -124,7 +156,6 @@ export default {
         event_type: "",
         revenue_generation: ""
       },
-      landscape: true,
       reactive: true,
       status: ["Pending", "Not started", "In progress", "Finished"],
     };
